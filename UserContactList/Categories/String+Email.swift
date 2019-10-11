fd511b44d12071f1732b72f795c2fae5bf77f5fb
Apple <apple@Apples-Mac-mini-2.local>
2019-10-11 10:27:36 +0530

    first commit

---
+++ UserContactList/Categories/String+Email.swift
@@ -0,0 +1,41 @@
+//
+//  String+Email.swift
+//  AddressBookContacts
+//
+//  Created by Ignacio Nieto Carvajal on 21/4/16.
+//  Copyright © 2016 Ignacio Nieto Carvajal. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    func isEmail() -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]+$", options: NSRegularExpression.Options.caseInsensitive)
+            return regex.firstMatch(in: self, options: [], range: NSMakeRange(0, self.count)) != nil
+        } catch { return false }
+    }
+    
+    func containsAlphabets() -> Bool {
+        //Checks if all the characters inside the string are alphabets
+        let set = CharacterSet.letters
+        return self.utf16.contains( where: {
+            guard let unicode = UnicodeScalar($0) else { return false }
+            return set.contains(unicode)
+        })
+    }
+    
+    subscript(r: Range<Int>) -> String? {
+        get {
+            let stringCount = self.count as Int
+            if (stringCount < r.upperBound) || (stringCount < r.lowerBound) {
+                return nil
+            }
+            let startIndex = self.index(self.startIndex, offsetBy: r.lowerBound)
+            let endIndex = self.index(self.startIndex, offsetBy: r.upperBound - r.lowerBound)
+            return String(self[(startIndex ..< endIndex)])
+        }
+    }
+    
+}
---
+++ UserContactList/ViewControllers/ViewController.swift
@@ -0,0 +1,129 @@
+//
+//  ViewController.swift
+//  UserContactList
+//
+//  Created by Apple on 10/10/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+
+import UIKit
+import Contacts
+
+
+fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l < r
+    case (nil, _?):
+        return true
+    default:
+        return false
+    }
+}
+
+fileprivate func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
+    switch (lhs, rhs) {
+    case let (l?, r?):
+        return l > r
+    default:
+        return rhs < lhs
+    }
+}
+
+
+@available(iOS 9.0, *)
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+   
+    @IBOutlet weak var noContactsLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // data
+    var contactStore = CNContactStore()
+    var contacts = [ContactEntry]()
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UINib(nibName: "ContactTableViewCell", bundle: nil), forCellReuseIdentifier: "ContactTableViewCell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.isHidden = true
+        noContactsLabel.isHidden = false
+        noContactsLabel.text = "Retrieving contacts..."
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        requestAccessToContacts { (success) in
+            if success {
+                self.retrieveContacts({ (success, contacts) in
+                    self.tableView.isHidden = !success
+                    self.noContactsLabel.isHidden = success
+                    if success && contacts?.count > 0 {
+                        self.contacts = contacts!
+                        self.tableView.reloadData()
+                    } else {
+                        self.noContactsLabel.text = "Unable to get contacts..."
+                    }
+                })
+            }
+        }
+    }
+    
+    
+    func requestAccessToContacts(_ completion: @escaping (_ success: Bool) -> Void) {
+        let authorizationStatus = CNContactStore.authorizationStatus(for: CNEntityType.contacts)
+        
+        switch authorizationStatus {
+        case .authorized: completion(true) // authorized previously
+        case .denied, .notDetermined: // needs to ask for authorization
+            self.contactStore.requestAccess(for: CNEntityType.contacts, completionHandler: { (accessGranted, error) -> Void in
+                completion(accessGranted)
+            })
+        default: // not authorized.
+            completion(false)
+        }
+    }
+    
+    func retrieveContacts(_ completion: (_ success: Bool, _ contacts: [ContactEntry]?) -> Void) {
+        var contacts = [ContactEntry]()
+        do {
+            let contactsFetchRequest = CNContactFetchRequest(keysToFetch: [CNContactGivenNameKey as CNKeyDescriptor, CNContactFamilyNameKey as CNKeyDescriptor, CNContactImageDataKey as CNKeyDescriptor, CNContactImageDataAvailableKey as CNKeyDescriptor, CNContactPhoneNumbersKey as CNKeyDescriptor, CNContactEmailAddressesKey as CNKeyDescriptor])
+            contactsFetchRequest.sortOrder = CNContactSortOrder.userDefault
+            try contactStore.enumerateContacts(with: contactsFetchRequest, usingBlock: { (cnContact, error) in
+                if let contact = ContactEntry(cnContact: cnContact) { contacts.append(contact)}
+            })
+            completion(true, contacts)
+        } catch {
+            completion(false, nil)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // UITableViewDataSource && Delegate methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
+    
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactTableViewCell", for: indexPath) as! ContactTableViewCell
+        let entry = contacts[(indexPath as NSIndexPath).row]
+        cell.configureWithContactEntry(entry)
+        cell.layoutIfNeeded()
+
+        return cell
+    }
+
+}
